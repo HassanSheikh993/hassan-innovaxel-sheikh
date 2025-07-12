34bf2cb15255294c06b953df23e561016de6ca68
HassanSheikh993 <hassansheikh993@gmail.com>
2025-07-12 18:08:43 +0500

    enable cors for frontend connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import { router } from "./route.js";
 
 dotenv.config()
@@ -10,7 +11,7 @@ const port = process.env.PORT;
 const url = process.env.DB_URL;
 
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
